a4861fea6e9dd97e1255bc69d7fc5216f2d27098
Ioana Nasui <ioana.nasui@intern.arobs.com>
2021-04-22 17:46:47 +0300

    [22.04.2021] - modified .gitignore and added main and 20.04.21 folder

---
+++ 20.04.21/main.py
@@ -0,0 +1,8 @@
+# define the function for data_types
+def data_type():
+    spam = 1 #this is a variable
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
